cd70b4fd8c63f0afa10aedac8a9031ba8d709e64
ashvn24 <ashwinashi6@gmail.com>
2024-01-04 17:35:46 +0530

    avg

--- python.py
+++ python.py
@@ -5,8 +5,19 @@
 # a,b=input('a and b').split()
 # print(a,b)
 
-a,b = input('enter the values of a and b ').split()
+# a,b = input('enter the values of a and b ').split()
 
-a,b=b,a
+# a,b=b,a
 
-print(a,b)
+# print(a,b)
+
+num = int(input("enter the numbers ?"))
+items = num.split()
+
+val = [int(num) for num in items]
+
+total = 0
+
+for i in val:
+    total += val
+avg = total/val.length()
