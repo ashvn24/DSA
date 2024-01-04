0b10785fbd5166791c11bab4b9f8c8f064ea7282
ashvn24 <ashwinashi6@gmail.com>
2024-01-04 15:52:08 +0530

    added jupyter

--- python.py
+++ python.py
@@ -1,1 +1,12 @@
-print("print('heloo')")
+# # print("print('heloo')")
+# a= input('name?')
+# print(a)
+
+# a,b=input('a and b').split()
+# print(a,b)
+
+a,b = input('enter the values of a and b ').split()
+
+a,b=b,a
+
+print(a,b)
